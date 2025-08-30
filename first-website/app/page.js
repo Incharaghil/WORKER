1b36f5301714a63e165d9a8cfe1fcfed95dc4ceb
Devina Dian <10241022@student.itk.ac.id>
2025-08-31 00:22:08 +0800

    Update page.js

--- first-website/app/page.js
+++ first-website/app/page.js
@@ -1,28 +1,44 @@
-export default function page() {
-  return (
-    <div className="bg-amber-50">
-      {/* Heading Bar */}
-      <div>
-        <img src="https://i.pinimg.com/736x/be/d7/ca/bed7caf1e07eba9cca1a7af636e62e4e.jpg"/>
-        <div>
-          <h1 className="text-5xl text-black underline ">SIGMA BAKERY</h1>
-          <h2>Homemade Cake</h2>
-        </div>
-      </div>
-      {/* Halaman kontent */}
-      <div>
-        <h3>Our Menu</h3>
-        <ul>
-          <li>Lotus Cake</li>
+export default function Home() {
+    return (
+        <div className="bg-green-200 h-screen flex items-center justify-start px-12">
+            {/* Heading Bar */}
+                <img src="https://i.pinimg.com/736x/1e/a1/ac/1ea1ac0416f6fe8ebcfd6bb0fbae76c9.jpg"
+                     className="w-40 h-40 rounded-full object-cover"
+                     alt="Bakery"
+                />
+                <div className="ml-6">
+                    <h1 className="text-5xl text-black">SIGMA BAKERY</h1>
+                    <h2 className="text-xl  text-black">Homemade Cake</h2>
+                </div>
+            </div>
+    )
+}
 
-        </ul>
-      </div>
+// export default function page() {
+//   return (
+//     <div className="bg-amber-50">
+//       {/* Heading Bar */}
+//       <div>
+//         <img src="https://i.pinimg.com/736x/be/d7/ca/bed7caf1e07eba9cca1a7af636e62e4e.jpg"/>
+//         <div>
+//           <h1 className="text-5xl text-black underline ">SIGMA BAKERY</h1>
+//           <h2>Homemade Cake</h2>
+//         </div>
+//       </div>
+//       {/* Halaman kontent */}
+//       <div>
+//         <h3>Our Menu</h3>
+//         <ul>
+//           <li>Lotus Cake</li>
+
+//         </ul>
+//       </div>
 
       
-    </div>
-  );
-}  
-// <div className="bg-emerald-200">
+//     </div>
+//   );
+// }  
+// // <div className="bg-emerald-200">
     //   <div className="">
     //     <h1 className="text-5xl">SIGMA BAKERY</h1>
     //   </div>
