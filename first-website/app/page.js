733d1845d2b51629cbb8e954efb0a5e9ccd86b5b
Inchaaaa <150126039+Incharaghil@users.noreply.github.com>
2025-08-30 20:39:07 +0800

    fix: upload isi first-website

---
+++ first-website/app/page.js
@@ -0,0 +1,45 @@
+export default function page() {
+  return (
+    <div className="bg-amber-50">
+      {/* Heading Bar */}
+      <div>
+        <img src="https://i.pinimg.com/736x/be/d7/ca/bed7caf1e07eba9cca1a7af636e62e4e.jpg"/>
+        <div>
+          <h1 className="text-5xl text-black underline ">SIGMA BAKERY</h1>
+          <h2>Homemade Cake</h2>
+        </div>
+      </div>
+      {/* Halaman kontent */}
+      <div>
+        <h3>Our Menu</h3>
+        <ul>
+          <li>Lotus Cake</li>
+
+        </ul>
+      </div>
+
+      
+    </div>
+  );
+}  
+// <div className="bg-emerald-200">
+    //   <div className="">
+    //     <h1 className="text-5xl">SIGMA BAKERY</h1>
+    //   </div>
+      
+    //   <img
+    //     src="https://i.pinimg.com/1200x/6b/57/b9/6b57b9b35036cbe5b917fd07f8227404.jpg"
+    //     alt="Hedy Lamarr"
+    //     className="photo"
+    //   />
+    //   <div className="text-lime-200">
+    //     <h2 className="text-stone-800">About:</h2>
+    //     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    //       incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    //       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+    //       irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    //       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+    //       deserunt mollit anim id est laborum </p>
+
+    //   </div>
+    // </div>
---
+++ first-website/components/ProfileBar.js
@@ -0,0 +1,14 @@
+export default function Profilbar() {
+    return (
+        <div>
+            <div>
+                <img/>
+                <div>
+                    <p>Chelsea Immanuela</p>
+                    <p>Jakarta</p>
+                </div>
+            </div>
+            <img/>
+        </div>
+    )
+}
