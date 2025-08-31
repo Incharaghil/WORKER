229359bbe7ac3099c6a0ed8de4783c2088a5530e
Devina Dian <10241022@student.itk.ac.id>
2025-08-31 23:22:26 +0800

    Update page.js

--- first-website/app/page.js
+++ first-website/app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
-        <div className="bg-amber-100">
+        <div className="bg-white">
             {/* Header */}
-            <div className="bg-green-200 flex text-center justify-start">
+            <div className="bg-green-100 flex text-center justify-start">
                 <img 
                     src="https://i.pinimg.com/736x/1e/a1/ac/1ea1ac0416f6fe8ebcfd6bb0fbae76c9.jpg"
                     className="w-100 h-100 rounded-full object-cover mx-auto"
@@ -14,11 +14,21 @@ export default function Home() {
                 </div>
             </div>
 
-            {/* Main Content */}
-            <div className="bg-amber-50 p-12">
-                <div className="bg-orange-100 rounded-2xl mx-auto">
+            {/* About Section */}
+            <div className="text-center mt-6">
+              {/* <div className="bg-orange-200 p-4 rounded 2xl"> */}
+              <h2 className=" bg-orange-200 text-3xl text-black text-center mb-6 ml-200 mr-200 rounded-full">About Me</h2>
+              <h3 className="text-xl text-center about-container text-black mb-7"> Selamat Datang di SIGMA BAKERY! Kami adalah toko kue rumahan yang menyediakan berbagai macam kue lezat. Di sisi, setiap harinya kue dibuat dengan menggunakan bahan-bahan terbaik. Kami menerima segala jenis pemesanan dengan desain yang dapat disesuaikan. Karena tidak ada yang lebih memuaskan dari melihat pelanggan yang tersenyum puas setelah menikmati kue buatan kami.
+              </h3>
+              {/* </div> */}
             </div>
-                <h2 className="bg-orange-200 text-3xl text-black text-center mb-6 ml-200 mr-200 rounded-full">OUR MENU</h2>
+
+
+            {/* Main Content */}
+            <div className="bg-white p-12">
+                <div className="bg-orange-100 rounded-2xl mx-auto">
+                </div>
+                <h2 className="bg-orange-200 text-3xl text-black text-center mb-6 ml-200 mr-200 rounded-full">Our Menu</h2>
                 <div className="flex object-center">
                   <div className="ml-57 mr-50 bg-rose-200 rounded-xl">
                     <img 
@@ -28,7 +38,7 @@ export default function Home() {
                     <h3 className="text-4xl text-black">Lotus Cake</h3>
                     <h3 className="text-4xl text-black ml-12">$10</h3>
                   </div>
-                  <div className="ml-50 mr-50 bg-rose-200 rounded-xl">
+                  <div className="ml-62 mr-50 bg-rose-200 rounded-xl">
                     <img 
                       src="https://i.pinimg.com/736x/ac/89/e6/ac89e6c0e79023b2e0684901d1992ee9.jpg"
                       className="w-40 h-40 rounded-full object-cover mx-auto"
@@ -49,50 +59,3 @@ export default function Home() {
         </div>
     )
 }
-
-
-// export default function page() {
-//   return (
-//     <div className="bg-amber-50">
-//       {/* Heading Bar */}
-//       <div>
-//         <img src="https://i.pinimg.com/736x/be/d7/ca/bed7caf1e07eba9cca1a7af636e62e4e.jpg"/>
-//         <div>
-//           <h1 className="text-5xl text-black underline ">SIGMA BAKERY</h1>
-//           <h2>Homemade Cake</h2>
-//         </div>
-//       </div>
-//       {/* Halaman kontent */}
-//       <div>
-//         <h3>Our Menu</h3>
-//         <ul>
-//           <li>Lotus Cake</li>
-
-//         </ul>
-//       </div>
-
-      
-//     </div>
-//   );
-// }  
-// // <div className="bg-emerald-200">
-    //   <div className="">
-    //     <h1 className="text-5xl">SIGMA BAKERY</h1>
-    //   </div>
-      
-    //   <img
-    //     src="https://i.pinimg.com/1200x/6b/57/b9/6b57b9b35036cbe5b917fd07f8227404.jpg"
-    //     alt="Hedy Lamarr"
-    //     className="photo"
-    //   />
-    //   <div className="text-lime-200">
-    //     <h2 className="text-stone-800">About:</h2>
-    //     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-    //       incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-    //       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-    //       irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    //       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-    //       deserunt mollit anim id est laborum </p>
-
-    //   </div>
-    // </div>
