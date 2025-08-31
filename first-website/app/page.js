8bd75949eb91c4c9389ce7750bcafa89026c3a2b
Devina Dian <10241022@student.itk.ac.id>
2025-09-01 06:56:58 +0800

    Update page.js

--- first-website/app/page.js
+++ first-website/app/page.js
@@ -5,19 +5,19 @@ export default function Home() {
             <div className="bg-green-100 flex text-center justify-start">
                 <img 
                     src="https://i.pinimg.com/736x/1e/a1/ac/1ea1ac0416f6fe8ebcfd6bb0fbae76c9.jpg"
-                    className="w-100 h-100 rounded-full object-cover mx-auto"
+                    className="w-60 h-60 rounded-full object-cover mx-auto"
                     alt="Bakery"
                 />
-                <div className="ml-6">
-                    <h1 className="text-7xl text-black mt-30 mr-150">SIGMA BAKERY</h1>
-                    <h2 className="text-4xl text-black mr-150">Homemade Cake</h2>
+                <div className="ml-30 mr-25">
+                    <h1 className="text-5xl text-black mt-20 mr-80">SIGMA BAKERY</h1>
+                    <h2 className="text-3xl text-black mr-80">Homemade Cake</h2>
                 </div>
             </div>
 
             {/* About Section */}
             <div className="text-center mt-6">
               {/* <div className="bg-orange-200 p-4 rounded 2xl"> */}
-              <h2 className=" bg-orange-200 text-3xl text-black text-center mb-6 ml-200 mr-200 rounded-full">About Me</h2>
+              <h2 className=" bg-orange-200 text-2xl text-black text-center mb-6 ml-155 mr-155 rounded-full">About Me</h2>
               <h3 className="text-xl text-center about-container text-black mb-7"> Selamat Datang di SIGMA BAKERY! Kami adalah toko kue rumahan yang menyediakan berbagai macam kue lezat. Di sisi, setiap harinya kue dibuat dengan menggunakan bahan-bahan terbaik. Kami menerima segala jenis pemesanan dengan desain yang dapat disesuaikan. Karena tidak ada yang lebih memuaskan dari melihat pelanggan yang tersenyum puas setelah menikmati kue buatan kami.
               </h3>
               {/* </div> */}
@@ -28,31 +28,39 @@ export default function Home() {
             <div className="bg-white p-12">
                 <div className="bg-orange-100 rounded-2xl mx-auto">
                 </div>
-                <h2 className="bg-orange-200 text-3xl text-black text-center mb-6 ml-200 mr-200 rounded-full">Our Menu</h2>
+                <h2 className="bg-orange-200 text-2xl text-black text-center mb-6 ml-155 mr-155 rounded-full">Our Menu</h2>
                 <div className="flex object-center">
-                  <div className="ml-57 mr-50 bg-rose-200 rounded-xl">
+                  <div className="ml-21 mr-10 bg-rose-200 rounded-xl w-64 p-4">
                     <img 
                       src="https://i.pinimg.com/1200x/00/20/89/002089eec1463a41e3c93fbdb12bf56d.jpg"
-                      className="w-40 h-40 rounded-full object-cover mx-auto "
+                      className="w-40 h-40 rounded-full object-cover mx-auto"
                   />
-                    <h3 className="text-4xl text-black">Lotus Cake</h3>
-                    <h3 className="text-4xl text-black ml-12">$10</h3>
+                    <h3 className="text-2xl text-center text-black mt-2">Lotus Cake</h3>
+                    <h3 className="text-2xl text-center text-black">$10</h3>
                   </div>
-                  <div className="ml-62 mr-50 bg-rose-200 rounded-xl">
+                  <div className="ml-10 mr-10 bg-rose-200 rounded-xl w-64 p-4">
                     <img 
                       src="https://i.pinimg.com/736x/ac/89/e6/ac89e6c0e79023b2e0684901d1992ee9.jpg"
                       className="w-40 h-40 rounded-full object-cover mx-auto"
                   />
-                    <h3 className="text-4xl text-black">Strawberry Cake</h3>
-                    <h3 className="text-4xl text-black ml-23">$20</h3>
+                    <h3 className="text-2xl text-center text-black mt-2">Strawberry Cake</h3>
+                    <h3 className="text-2xl text-center text-black">$20</h3>
                   </div>
-                  <div className="ml-50 mr-50 bg-rose-200 rounded-xl">
+                  <div className="ml-10 mr-10 bg-rose-200 rounded-xl w-64 p-4">
+                    <img 
+                      src="https://i.pinimg.com/736x/41/f4/d5/41f4d5c2aec9e488a6c169b327d20e94.jpg"
+                      className="w-40 h-40 rounded-full object-cover mx-auto"
+                  />
+                    <h3 className="text-2xl text-center text-black mt-2">Matcha Cake</h3>
+                    <h3 className="text-2xl text-center text-black">$20</h3>
+                  </div>
+                  <div className="ml-10 mr-10 bg-rose-200 rounded-xl w-64 p-4">
                     <img 
                       src="https://i.pinimg.com/1200x/01/8a/43/018a4362d14d1dd33771ed0c1af60d0c.jpg"
                       className="w-40 h-40 rounded-full object-cover mx-auto"
                   />
-                    <h3 className="text-4xl text-black">Peach Cake</h3>
-                    <h3 className="text-4xl text-black ml-15">$15</h3>
+                    <h3 className="text-2xl text-center text-black mt-2">Peach Cake</h3>
+                    <h3 className="text-2xl text-center text-black">$15</h3>
                   </div>
                 </div>
             </div>
